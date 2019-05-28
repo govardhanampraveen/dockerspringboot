b4281b57716aab4f90c7d35e12d60ce767252daa
bhanu <bhanu.yandra@gmail.com>
2019-05-28 23:11:55 +0100

    Docker Spring boot sample

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# dockerspringboot
-Running Springboot application on Docker
+# Springboot application on docker
+
+### Build project
+mvn clean install
+
+### Build docker image
+
+docker build -t byservices/springboot .
+
+### Run docker 
+
+docker run -d -p 8085:8085 byservices/springboot
+
+### Testing
+Open URL http://localhost:8085/serverip
+
+
+
---
+++ src/main/java/com/byservices/dockerspringboot/rest/LoginController.java
@@ -0,0 +1,24 @@
+package com.byservices.dockerspringboot.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/serverip")
+    public String hello(HttpServletRequest request) {
+
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+            return ip.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
